2b971127615b5c33cb9986f06f474f6594e2a90a
TerentievaEizaveta <119036066+TerentievaEizaveta@users.noreply.github.com>
2023-04-18 23:31:20 +0300

    Delete

--- homework8.py
+++
@@ -1,9 +0,0 @@
-#Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data. 
-#Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
-import pandas as pd
-df = pd.read_csv('C:\\Users\\User\\Desktop\\HW\\Homework_Py8\\california_housing_train.csv')
-print(df.head())
-print(round(df[df['population'] <= 500]['median_house_value'].mean(), 2))
-
-#Задача 42: Узнать какая максимальная households в зоне минимального значения population.
-print(df[df['population'] == df['population'].min()]['households'].max())
